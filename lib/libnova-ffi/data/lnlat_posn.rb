83fe7cf8bed32bd223e8bd7a87fd11dd3a614810
Ho-Sheng Hsiao <hosheng.hsiao@gmail.com>
2013-05-25 15:38:37 -0700

    [data] Added Data::LnLatPosn (ln_lnlat_posn)

---
+++ lib/libnova-ffi/data/lnlat_posn.rb
@@ -0,0 +1,19 @@
+require 'ffi'
+
+module LibNova
+  module Data
+    class LnLatPosn < FFI::Struct
+      # Ecliptical (or celestial) Longitude and Latitude.
+
+      # The Ecliptical (or celestial) Latitude and Longitude of and object.
+      # Angles are expressed in degrees. East is positive, West negative.
+
+      # Maps to struct ln_lnlat_posn
+      # http://libnova.sourceforge.net/structln__lnlat__posn.html
+
+      layout :lng, :double, # longitude (degrees)
+             :lat, :double, # latitude (degrees)
+
+    end
+  end
+end
